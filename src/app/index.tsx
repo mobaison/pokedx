f1d57dfa7dc373bae83fff416d0c4cadc97011a9
mobaison <mobaison21@gmail.com>
2026-03-26 21:33:35 +0530

    init to win

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this  hello screen.</Text>
+      <Text>Press Cmd + R to reload, Cmd + D or shake for dev menu.</Text>  
+    </View>
+  );
+}
